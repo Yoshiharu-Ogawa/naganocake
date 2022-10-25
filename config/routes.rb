102b000f85bf61b98c27b7dfa4c9e6803455901e
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-10-25 08:52:23 +0000

    [Add] routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,47 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
 }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #会員側のルーティング
+  scope module: :public do
+    resources :addresses, only: [:new, :create, :destroy ]
+
+    resources :cart_items, only: [:index, :create, :update, :destroy ]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    resources :customers, only: [:show, :edit, :update ]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/reject_user' => 'customers#reject_user'
+
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about'
+
+    resources :items, only: [:index, :show ]
+
+    resources :orders, only: [:new, :create, :index, :show ]
+    get 'orders/complete' => 'orders#complete'
+    post 'orders/confirm' => 'orders#confirm'
+
+    #resources :registrations, only: [:new, :create ]
+
+    #resources :sessions, only: [:new, :create, :destroy ]
+  end
+  #管理者側のルーティング
+  namespace :admin do
+
+    resources :customers, only: [:index, :show, :edit, :update ]
+
+    resources :genres, only: [:index, :edit, :create, :update ]
+
+    resources :homes, only: [:top ]
+
+    resources :items, only: [:new, :show, :index, :edit, :create, :update ]
+
+    resources :orders, only: [:show, :update ]
+
+    resources :order_details, only: [:update ]
+
+    #resources :sessions, only: [:new, :create, :destroy ]
+
+  end
 end
