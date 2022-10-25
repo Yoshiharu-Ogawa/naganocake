52b6f55a3168b72c66c78f8a6fe3e61a02a6a95c
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-10-25 09:05:55 +0000

    [Add] seed to admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Admin.create!(
+  email: 'gworijob@wrghtw24',
+  password: 'baorho42oi5hog89hs'
+  )
