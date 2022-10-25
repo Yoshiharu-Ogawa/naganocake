ca56e2be1708027e9c8bbe150514d83ef5a19578
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-10-25 08:38:31 +0000

    [Add] View

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +20,28 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】そのアカウントのis_deletedの中身（trueかfalse)を1件取得
+      @is_deleted = @customer.is_deleted
+      ## trueだった場合、退会しているのでサインアップ画面に遷移する
+      if @is_deleted == true
+        redirect_to 'new_registration_path'
+      end
+      ## falseだった場合、退会していないのでそのままcreateアクションを実行させる
+      if @is_deleted == false
+        render :create
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
