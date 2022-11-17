4e6da5211f27ceaa66e319b9137834a76972e2bb
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-11-17 14:50:47 +0000

    [Add] order_new

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,29 +1,45 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
+
     # @cart_item = CartItem.new
     @total = 0
   end
 
   def create
+    # 新しく追加するカートアイテム
     @cart_item = CartItem.new(cart_item_params)
-    @cart_items = CartItem.all
+    # すでにあるカートアイテム
+    @cart_items = current_customer.cart_items
     @cart_items.each do |cart_item|
+      # もし、新しく追加するカートアイテムとすでにあるカートアイテムのitem_idが一緒なら以下を実行
       if cart_item.item_id == @cart_item.item_id
+        # 新しいカートアイテムの個数とすでにあるカートアイテムの個数を合計してnew_amountに代入する
         new_amount = cart_item.amount.to_i + @cart_item.amount.to_i
+        # amountカラムだけupdateする
         cart_item.update(amount: new_amount)
+        # 新しく作ってしまったカートアイテムを削除する
         @cart_item.destroy
         redirect_to cart_items_path
+        # ここで処理を終わらせるためのreturn
         return
       end
     end
-      cart_item = CartItem.new(cart_item_params)
+      # cart_item = CartItem.new(cart_item_params)
       # cart_itemを保存するときに自分のcustomer_idも保存しなければならないため、customer_idにはcurrent_customerのidが入るという記述
-      cart_item.customer_id = current_customer.id
-      cart_item.save
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
       redirect_to cart_items_path
   end
+
+  # def find_byを使ったときの処理
+    # 新しく追加するカートアイテム
+    # @cart_item = CartItem.new(cart_item_params)
+    # すでにあるカートアイテム
+    # @cart_items = current_customer.cart_items
+  #   if CartItem.find_by(item_id: @cart_items.item_id) == CartItem.find_by(item_id: @cart_item.item_id)
+  # end
 
   def update
     cart_item = CartItem.find(params[:id])
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,31 +1,35 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
-    @order = Order.new 
-  end 
-  
+    @order = Order.new
+
+  end
+
   def confirm
     @order = Order.new(order_params)
-  end 
-  
-  def complete 
-  end 
-  
-  def create 
-  end 
-  
-  def index 
-    @orders = Order.all
-  end 
-  
-  def show 
-    @order = Order.find(params[:id])
-  end 
-  
-  private
-  
-  def order_params
-    params.require(:order).permit(:delivery_postal_code, :delivery_address, :delivery_name, :total_payment, :freight, :payment_method, :order_status, :customer_id)
+    
+
   end
-  
+
+  def complete
+  end
+
+  def create
+  end
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+
+  def order_params
+    # params.require(:order).permit(:delivery_postal_code, :delivery_address, :delivery_name, :total_payment, :freight, :payment_method, :order_status, :customer_id)
+    params.require(:order).permit(:payment_method)
+  end
+
 end
