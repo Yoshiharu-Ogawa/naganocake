8f5a49b82b8171dd09438382463f01ae7392c60c
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-11-20 14:43:58 +0000

    [Add] order_confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,14 +7,29 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    
+    @cart_items = current_customer.cart_items
+    @total = 0
+    if params[:order][:select_address] == "0"
+      @order.delivery_postal_code = current_customer.postal_code
+      @order.delivery_address = current_customer.address
+      @order.delivery_name = current_customer.last_name + current_customer.first_name
+    # elsif params[:order][:select_adderss] == 1
+    elsif params[:order][:select_address] == "2"
+      @order.delivery_postal_code = params[:order][:delivery_postal_code]
+      @order.delivery_address = params[:order][:delivery_address]
+      @order.delivery_name = params[:order][:delivery_name]
+    end
 
-  end
-
-  def complete
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    redirect_to orders_complete_path
+  end
+
+  def complete
   end
 
   def index
@@ -28,8 +43,8 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    # params.require(:order).permit(:delivery_postal_code, :delivery_address, :delivery_name, :total_payment, :freight, :payment_method, :order_status, :customer_id)
-    params.require(:order).permit(:payment_method)
+    params.require(:order).permit(:payment_method, :delivery_postal_code, :delivery_address, :delivery_name, :total_payment, :freight, :payment_method, :order_status, :customer_id,)
+    # params.require(:order).permit(:payment_method, :delivery_postal_code, :delivery_address,:delivery_name)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,10 @@ class Order < ApplicationRecord
 
   belongs_to :customer
 
+  def subtotal
+    item.add_tax_price * amount
+  end
+
   enum payment_method: {credit_card: 0, transfer: 1 }
   enum order_status:{payment_waiting: 0, payment_confirmation: 1, in_production: 2, delivery_preparation: 3, delivered: 4 }
 
