724a89f6506f8ebe57a03af0725595ef018ab99c
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-11-27 06:47:50 +0000

    [Add] admin_orders_show order_status and production_status coordination function

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,15 +1,33 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    order_detail = OrderDetail.find(params[:id])
-    order_detail.update(order_detail_params)
-    redirect_to admin_order_path(order_detail.order.id)
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
+
+
+    # 制作ステータスが製作中だったとき、注文ステータスを製作中に更新する
+    if @order_detail.production_status == 'in_production'
+      #@order.order_status = 2
+      @order.update(order_status: 2)
+    end
+
+    if @order_details.count == @order_details.where(production_status: 'production_complete').count
+      @order.update(order_status: 3)
+    end
+
+    redirect_to admin_order_path(@order_detail.order.id)
   end
 
   private
 
   def order_detail_params
     params.require(:order_detail).permit(:price_including_tax, :quantity, :production_status )
+  end
+
+  def order_params
+    params.require(:order).permit(:order_status )
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,6 +9,10 @@ class Admin::OrdersController < ApplicationController
   def update
     order = Order.find(params[:id])
     order.update(order_params)
+    if order.order_status == 'payment_confirmation'
+      @order_details = order.order_details
+      @order_details.update_all(production_status: 1 )
+    end
     redirect_to admin_order_path(order.id)
   end
 
@@ -16,6 +20,10 @@ class Admin::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:delivery_postal_code, :delivery_address, :delivery_name, :total_payment, :freight, :payment_method, :order_status )
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:price_including_tax, :quantity, :production_status )
   end
 
 end
